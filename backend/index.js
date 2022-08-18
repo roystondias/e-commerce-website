42bdfd0728166b06dfb82088df83f890e68f2f8a
royston <diasroyston034@gmail.com>
2022-08-18 11:50:54 +0530

    backend

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3000;
+const apiData= require("./data.json");
+
+app.use(cors());
+
+app.get("/", (req, res)=>{
+    res.send("hello I am live");
+})
+
+app.get("/api", (req, res)=>{
+    res.send(apiData)
+})
+
+app.listen(port, ()=>{
+    console.log("i am live again");
+})
